6e2b97f0fba461db3b76388ddf813089dd34a485
Gary Lin <garyqwertyuiop@gmail.com>
2020-11-30 09:18:31 +0800

    make a instance method into class method

--- metaprogramming/code_write_code/attr_checker.rb
+++ metaprogramming/code_write_code/attr_checker.rb
@@ -1,19 +1,41 @@
 require 'test/unit'
-class Object
-  def self.attr_checked(attribute, &validation)
-    self.class_eval do
-      attr_accessor attribute
+# class Object
+#   def self.attr_checked(attribute, &validation)
+#     self.class_eval do
+#       attr_accessor attribute
 
-      define_method("#{attribute}=") do |arg|
-        fail 'Invalid attribute' unless arg
-        fail 'Invalid attribute' unless validation.call(arg)
-        instance_variable_set("@#{attribute}", arg)
+#       define_method("#{attribute}=") do |arg|
+#         fail 'Invalid attribute' unless arg
+#         fail 'Invalid attribute' unless validation.call(arg)
+#         instance_variable_set("@#{attribute}", arg)
+#       end
+#     end
+#   end
+# end
+
+module CheckedAttribute
+  def self.included(klass)
+    klass.extend SubModule
+  end
+
+  module SubModule
+    def attr_checked(attribute, &validation)
+      self.class_eval do
+        attr_accessor attribute
+  
+        define_method("#{attribute}=") do |arg|
+          fail 'Invalid attribute' unless arg
+          fail 'Invalid attribute' unless validation.call(arg)
+          instance_variable_set("@#{attribute}", arg)
+        end
       end
     end
   end
 end
 
 class Person
+  include CheckedAttribute
+  
   attr_checked :age do |v|
     v >= 18
   end
